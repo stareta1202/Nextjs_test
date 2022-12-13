2209f57e240c0306decc45abd2d8015e98676e5e
stareta1202 <stareta1202@gmail.com>
2022-12-13 23:48:49 +0900

    navigation 추가

--- nextjs-intro/pages/movieclone.tsx
+++ nextjs-intro/pages/movieclone.tsx
@@ -1,38 +1,35 @@
 import exp from "constants";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Seo from "./components/Seo";
 
 
-export default function Movie() {
-    const [movies, setMovies] = useState([])
-    useEffect(() => {
-        (async() => {
-            const { results } = await (
-                await fetch(
-                    `/api/movies`
-                    )
-            ).json()
-            setMovies(results)            
-
-        })()
-        console.log("its ininin222")
-    }, [])
+export default function Movie({ results }) {
+    
     return (
         <div className="container">
-          <Seo title="Home" />
-          {!movies && <h4>Loading...</h4>}
-          {movies?.map((movie) => (
-            <div className="movie" key={movie.id}>
-              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-              <h4>{movie.original_title}</h4>
-            </div>
-          ))}
-          <style jsx>{`
+            <Seo title="Home" />
+            {results?.map((movie) => (
+
+                <div className="movie" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <h4>
+                        <Link href={`/movies/${movie.id}`} >
+                             {movie.original_title} 
+                        </Link>
+                    </h4>
+                </div>
+
+            ))}
+            <style jsx>{`
             .container {
               display: grid;
               grid-template-columns: 1fr 1fr;
               padding: 20px;
               gap: 20px;
+            }
+            .movie {
+                cursor: pointer;
             }
             .movie img {
               max-width: 100%;
@@ -49,5 +46,19 @@ export default function Movie() {
             }
           `}</style>
         </div>
-      )
+    )
+}
+
+export async function getServerSideProps() {
+    const { results } = await (
+        await fetch(
+            `http://localhost:3000/api/movies`
+        )
+    ).json()
+    return {
+        props: {
+            results,
+        },
+    }
+
 }
